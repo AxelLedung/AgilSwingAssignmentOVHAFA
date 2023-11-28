33ad964c1298873d1b1f0a2d4d6498770a5f989a
AxelLedung <axel.ledung@gmail.com>
2023-11-28 14:27:01 +0100

    Revert merge due to duplicates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,5 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        CustomerManager customerManager = new CustomerManager();
-        LoginScreen loginScreen = new LoginScreen(customerManager.getCustomerList());
     }
 }
