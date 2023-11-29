9879b86ddb7482695f7ad085e095cab67f0a98aa
AxelLedung <axel.ledung@gmail.com>
2023-11-29 16:56:56 +0100

    Added customer panel again, Changes to Customer panel and product

--- src/main/java/org/example/CustomerPanel.java
+++ src/main/java/org/example/CustomerPanel.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class CustomerPanel {
     private JPanel jPanel;
@@ -8,18 +11,47 @@ public class CustomerPanel {
     private JButton checkoutButton;
     private JButton backButton;
     private JButton addProductButton;
-    private DefaultListModel defaultListModel = new DefaultListModel();
+    private JList jCheckoutList;
+    private DefaultListModel productListModel = new DefaultListModel();
+    private DefaultListModel checkoutListModel = new DefaultListModel();
+    public ArrayList<Product> productsToCheckoutArrayList = new ArrayList<Product>();
     public CustomerPanel(ProductManager productManager) {
         JFrame jFrame = new JFrame();
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.setSize(500, 500);
         jFrame.setContentPane(jPanel);
         jFrame.setVisible(true);
-        jProductList.setModel(defaultListModel);
+        jProductList.setModel(productListModel);
+        jCheckoutList.setModel(checkoutListModel);
 
         for (int i = 0; i < productManager.productArrayList.size(); i++) {
-            defaultListModel.addElement(productManager.productArrayList.get(i).GetDescription());
+            productListModel.addElement(productManager.productArrayList.get(i).GetDescription());
         }
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Back to Login screen
+                LoginPanel loginPanel = new LoginPanel();
+                jFrame.setVisible(false);
+            }
+        });
+        checkoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //Create a checkout window and send: productsToCheckoutArrayList to Checkout Window
+            }
+        });
+        addProductButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int index = jProductList.getSelectedIndex();
+                if (index >= 0) {
+                    productsToCheckoutArrayList.add(productManager.productArrayList.get(index));
+                    checkoutListModel.addElement(productManager.productArrayList.get(index).GetDescription());
+                }
+            }
+        });
     }
 
 }
