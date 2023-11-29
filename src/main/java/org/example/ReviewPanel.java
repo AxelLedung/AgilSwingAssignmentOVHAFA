b21a9adb93688a24cc4f281c499f69402ff7b5c9
OskarKarlssonGIT <150839400+OskarKarlssonGIT@users.noreply.github.com>
2023-11-29 02:39:14 +0100

    Added review panel

---
+++ src/main/java/org/example/ReviewPanel.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ReviewPanel extends JFrame {
+    private JButton tempProductButton;
+    private JLabel rvLabel2;
+    private JCheckBox a5CheckBox;
+    private JCheckBox a4CheckBox;
+    private JCheckBox a3CheckBox;
+    private JCheckBox a2CheckBox;
+    private JCheckBox a1CheckBox;
+    private JButton submitButton;
+    private JTextArea textArea1;
+
+    public ReviewPanel () {
+        setContentPane(ReviewPanel1);
+        setTitle("ReviewPanel");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+
+            }
+        }
+
+
+
+
+
