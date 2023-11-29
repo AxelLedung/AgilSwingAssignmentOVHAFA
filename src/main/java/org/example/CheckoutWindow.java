2ec35e063754e1c2cf57cdb000e9d3d990ff25a0
Krimpster <vicke43@gmail.com>
2023-11-29 16:30:28 +0100

    added checkout

--- src/main/java/org/example/CheckoutWindow.java
+++ src/main/java/org/example/CheckoutWindow.java
@@ -12,10 +12,18 @@ public class CheckoutWindow {
     private JList checkoutJList;
     private JButton makePurchaseButton;
     private JButton goBackToShopButton;
-
+    private JLabel costLabel;
     DefaultListModel listModel = new DefaultListModel<>();
+    private String name;
+    int finalCost;
+    ArrayList<Order> orderList = new ArrayList<>();
+    ArrayList<String> transferList = new ArrayList<>();
 
-    public CheckoutWindow(ArrayList<Customer> customerList, ArrayList<Product> checkoutList, ArrayList<Product> productList) {
+
+    public CheckoutWindow(String loggedCustomer, ArrayList<Customer> customerList, ArrayList<Product> checkoutList, ArrayList<Product> productList) {
+        for(Product p : checkoutList){
+            finalCost += p.getCost();
+        }
         checkoutJList.setModel(listModel);
         jFrame = new JFrame("Checkout");
         jFrame.setContentPane(checkoutWindow);
@@ -23,19 +31,39 @@ public class CheckoutWindow {
         jFrame.setVisible(true);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         checkoutListRefresh(checkoutList);
+        costLabel.setText("Final cost is: " + finalCost + " buckaroos.");
 
         goBackToShopButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                ShopWindow shopWindow = new ShopWindow(customerList, productList);
+                ShopWindow shopWindow = new ShopWindow(loggedCustomer, customerList, productList);
                 jFrame.dispose();
             }
         });
+        makePurchaseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for(Customer c : customerList) {
+                    if(c.getBalance() >= finalCost) {
+                        name = loggedCustomer;
+                        for (Product p : checkoutList) {
+                            transferList.add(p.getName());
+                        }
+                        orderList.add(new Order(name, finalCost, transferList));
+                        OrdersTest orders = new OrdersTest(loggedCustomer, customerList, checkoutList, productList, orderList);
+                        jFrame.dispose();
+                    }
+                    else{
+                        costLabel.setText("You don't have enough money to finalize the purchase.");
+                    }
+                }
+            }
+        });
     }
-    public void checkoutListRefresh(ArrayList<Product> checkoutList){
+    private void checkoutListRefresh(ArrayList<Product> checkoutList){
         listModel.removeAllElements();
         for(Product p : checkoutList){
-            listModel.addElement(p.getProductName());
+            listModel.addElement(p.getName() + "  Cost: " +  p.getCost());
         }
     }
 }
--- src/main/java/org/example/Shop.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Shop {
-    User currentUser = new Customer("Axel");
-    public Shop() {
-        ProductManager productManager = new ProductManager();
-        CustomerPanel customerPanel = new CustomerPanel(productManager);
-    }
-}
