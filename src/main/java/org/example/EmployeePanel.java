628f922bf2c09c6729e2339ecd369419970b63ea
7amada7mad <ahmad.hamad.sy99@gmail.com>
2023-11-29 19:54:46 +0100

    Added removing products functionallity into EmployeePanel

--- src/main/java/org/example/EmployeePanel.java
+++ src/main/java/org/example/EmployeePanel.java
@@ -13,20 +13,26 @@ public class EmployeePanel {
     private JLabel currentEmployeeLabel;
     private JList productsJlist;
     private JButton logoutButton;
+    private String currentEmployee;
     private DefaultListModel productListModel = new DefaultListModel();
 
-    public EmployeePanel(ProductManager productManager, Admin admin) {
+    public EmployeePanel(ProductManager productManager, Admin adminm, Employee currentEmployee) {
         JFrame jFrame = new JFrame();
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.setSize(600, 600);
         jFrame.setContentPane(employeePanel);
         jFrame.setVisible(true);
         productsJlist.setModel(productListModel);;
-
+        for (int i = 0; i < productManager.productArrayList.size(); i++) {
+            productListModel.addElement(productManager.productArrayList.get(i).GetDescription());
+        }
+        currentEmployeeLabel.setText("Välkommen "+currentEmployee.getUsername());
         removeProductButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            //
+            int index = productsJlist.getSelectedIndex();
+                productManager.productArrayList.remove(index);
+                productListModel.removeElementAt(index);
             }
         });
         editProductButton.addActionListener(new ActionListener() {
--- src/main/java/org/example/Shop.java
+++ src/main/java/org/example/Shop.java
@@ -6,5 +6,6 @@ public class Shop {
         Admin admin = new Admin();
         //LoginPanel loginPanel = new LoginPanel(admin, productManager);
         EmployeePanel employeePanel = new EmployeePanel(productManager, admin);
+//        CustomerPanel customerPanel = new CustomerPanel(productManager, admin);
     }
 }
