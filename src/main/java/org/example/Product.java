07624df20d846d3deb4aa38ea9a3df8c9b253e24
AxelLedung <axel.ledung@gmail.com>
2023-11-30 13:02:16 +0100

    Added Review button in CustomerPanel

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -44,11 +44,9 @@ public class Product {
     public void setCategory(String category) {
         this.category = category;
     }
-
     public int getQuantity() {
         return quantity;
     }
-
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
